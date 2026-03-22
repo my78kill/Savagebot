1f60ed1b465e30ce597bb0aa5927a33fe975cc0f
my78kill <aigirlfriend143you@gmail.com>
2026-03-22 22:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8686831676:AAEv0QkC3POSl5G-FA45SvNlwIxEa216IXw"
 
 # ⏱ timings
 DELETE_TIME = 1800   # 30 minutes
 WARN_DELETE = 7      # warning msg delete time
 
 # 🔗 bot username (for button)
-BOT_USERNAME = "YOUR_BOT_USERNAME"
+BOT_USERNAME = "SexySassyBot"
