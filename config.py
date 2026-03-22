541523e753787a69bddef4ebf39413d8131fd7cb
my78kill <aigirlfriend143you@gmail.com>
2026-03-22 21:36:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# ⏱ timings
+DELETE_TIME = 1800   # 30 minutes
+WARN_DELETE = 7      # warning msg delete time
+
+# 🔗 bot username (for button)
+BOT_USERNAME = "YOUR_BOT_USERNAME"
